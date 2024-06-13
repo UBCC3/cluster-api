825f43d0300f171aec5854d975f17e6ad253a27e
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-06-13 12:23:45 -0700

    [Develop] Write and remove sbatch scripts

--- cancel_job.py
+++ cancel_job.py
@@ -1,5 +1,6 @@
 import subprocess
 
-def find_job_id():
-    
+def find_job_id(db_job_id):
+    pass
 if __name__ == "__main__":
+    pass
--- submit_job.py
+++ submit_job.py
@@ -1,25 +1,32 @@
 import sys
 import json
 import subprocess
-
+import os
 # TODO: Add S3 Upload
 # TODO: Add clean up scripts
 def write_sbatch_script(job_name, command):
-    with open("submit_job.sh", "w") as file:
+    try:  
+        os.mkdir(job_name)  
+    except OSError as error:  
+        print(error) 
+    cur_path = os.path.join("./"+job_name)
+    with open(cur_path + "/submit_job.sh", "w") as file:
         file.write(f'''#!/bin/bash
         #SBATCH --job-name={job_name}
         #SBATCH --output={job_name}.out
         #SBATCH --error={job_name}.err
-        mkdir {job_name}
-        cd {job_name}
         {command}
 
-        cd ..
-        rm -r {job_name}
         ''')
+    with open(cur_path + "/clean_up.sh", "w") as file:
+        file.write(f'''#!/bin/bash
+        rm -r {job_name}
+                   ''')
 def submit_sbatch_script(script_path):
-    result = subprocess.run(['sbatch', script_path], capture_output=True, text=True)
+    result = subprocess.run(['sbatch', script_path  + "/submit_job.sh"], capture_output=True, text=True)
     print(result)
+    clean_up_result = subprocess.run(["sbatch", script_path + "/clean_up.sh"],capture_output=True, text=True)
+    print(clean_up_result)
 # NOTE: Input JSON cannot have any spaces in it
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -32,8 +39,8 @@ if __name__ == "__main__":
     job_wave_theory = job_input_data["waveTheory"]
     job_calculation_type = job_input_data["calculation"]
     job_solvent_effects = job_input_data["solventEffects"]
-    script_path = "./" + job_sql_id + "submit_job.sh"
-    write_sbatch_script("testing", 'echo "bruh"')
+    script_path = "./" + job_sql_id
+    write_sbatch_script(script_path, 'echo "test"')
     submit_sbatch_script(script_path)
 
 
