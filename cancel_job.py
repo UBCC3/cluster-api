27f87fb0d8646c96b301dfd3e57c63569dae3d64
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-06-17 12:10:30 -0700

    [Develop] add workflow for finding job_id and running scancel

--- cancel_job.py
+++ cancel_job.py
@@ -1,7 +1,19 @@
 import subprocess
-
-def find_job_id(db_job_id):
-    
-    pass
+import os
+def find_job_id(job_name):
+    current_path = os.path.join("./"+job_name)
+    slurm_job_id = ""
+    try:
+        with open(current_path + "/slurm_id.txt", "r") as file:
+            slurm_job_id = file.readLine()
+            cancel_command = [
+                "scancel", slurm_job_id
+            ]
+        subprocess.run(cancel_command, capture_output=True, text=True)
+        subprocess.run(["sbatch", current_path + "/clean_up.sh"],capture_output=True, text=True)
+    except FileNotFoundError as error:
+        raise error
+    return True
 if __name__ == "__main__":
-    pass
+    db_job_id = input()
+    find_job_id(db_job_id)
