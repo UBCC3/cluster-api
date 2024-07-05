72e2bff3e88d9d4165d3b4be66697204e62704d9
Lyka <lykawangg@gmail.com>
2024-07-05 01:45:35 -0700

    update the upload_result method

--- upload_result.py
+++ upload_result.py
@@ -1,12 +1,22 @@
-import json
 import os
 
 import requests
 
-from main import root_dir
 from util import log_error
 
 def upload_result(parameters):
+    """
+    Upload files directly to the S3 bucket using AWS presigned URLs
+    Input:
+        - parameters: a dictionary that contains:
+            - Type: the type of file to be uploaded (zip or json)
+            - JobID: job ID in the database
+            - PresignedResponse: the presigned URL with any required fields for the POST request
+            - root_dir: the root directory path
+    Output:
+        - A dictionary that contains the HTTP status code of the upload request
+    """
+    root_dir = parameters[root_dir]
     result_type = ""
     if parameters["Type"] == "zip":
         result_type = ".zip"
@@ -16,16 +26,17 @@ def upload_result(parameters):
         log_error(f'Invalid Type word: {parameters["Type"]}')
     db_job_id = parameters["JobID"]
     presigned_response = parameters["PresignedResponse"]
-    result_path = os.path.join(root_dir, db_job_id, f'{db_job_id}{result_type}')
+    job_dir = os.path.join(root_dir, db_job_id, f'{db_job_id}{result_type}')
     try:
-        with open(result_path, 'rb') as f:
-            files = {'file': (result_path, f)}
+        with open(job_dir, 'rb') as f:
+            files = {'file': (job_dir, f)}
             http_response = requests.post(presigned_response['url'], 
                                           data=presigned_response['fields'], 
                                           files=files)
-            return_data = {"status_code": http_response.status_code}
-            print(json.dumps(return_data))
+            return {"status_code": http_response.status_code}
     except requests.RequestException as e:
         log_error(f'HTTP request fail with {str(e)}')
+        # TODO: return something to the backend
     except Exception as e:
         log_error(f'Error {str(e)}')
+        # TODO: return something to the backend
