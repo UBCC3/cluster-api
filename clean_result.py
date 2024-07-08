cc493c18fcbc8490be2f0c8bd9cd6888307e7d65
ubchemica <168596924+ubchemica@users.noreply.github.com>
2024-07-08 03:53:31 -0700

    Update clean_result.py

--- clean_result.py
+++ clean_result.py
@@ -5,8 +5,8 @@ from util import clean_up
 def clean_result(parameters):
     db_job_id = parameters["id"]
     root_dir = parameters["root_dir"]
-    file_path = os.path.join(root_dir, db_job_id)
-    if clean_up(file_path):
+    job_dir = os.path.join(root_dir, db_job_id)
+    if clean_up(job_dir):
         return "{'status':'SUCCESS'}"
     else:
         return "{'status':'FAILURE'}"
