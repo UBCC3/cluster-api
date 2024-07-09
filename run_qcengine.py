a06c8756158fb0d5f07c29450555f8a30ba82f26
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-07-09 14:29:52 -0700

    Modify write_sbatch_script to run run_qcengine.py with arguments, Fix main.py try-except-else clauses

--- run_qcengine.py
+++ run_qcengine.py
@@ -1,2 +1,24 @@
 import qcelemental as qcel
 import qcengine as qcng
+import sys
+
+def execute_psi4(calculation_type, method, basisSet):
+    mol = qcel.models.Molecule.from_data("""
+    O  0.0  0.000  -0.129
+    H  0.0 -1.494  1.027
+    H  0.0  1.494  1.027
+    """)
+
+    inp = qcel.models.AtomicInput(
+        molecule=mol,
+        driver="energy",
+        model={"method": method, "basis": basisSet},
+        keywords={"scf_type": "df"}
+        )
+
+    ret = qcng.compute(inp, "psi4",return_dict=True)
+
+    print(ret)
+
+if __name__ == "__main__":
+    execute_psi4(sys.argv[1], sys.argv[2], sys.argv[3])
--- submit_job.py
+++ submit_job.py
@@ -46,8 +46,10 @@ def write_sbatch_script(job_dir: str, script_path: str, job_input_data: dict):
     module load psi4/1.9
 
     source {psi4_env_dir}
-
-    python3 {qcEngine_script_loc}
+    basisSet="{job_basis_set}"
+    method="{job_theory}"
+    calculationType="{job_calculation_type}"
+    python3 {qcEngine_script_loc} "$calculationType" "$method" "$basisSet"
 
         ''')
 
@@ -95,5 +97,6 @@ def submit_job(job_input_data: dict) -> None:
         clean_up_result = clean_up(job_dir)
         return {'status':'FAILURE'}
     else:
-        return submit_sbatch_script(job_dir, script_path)
+        # return submit_sbatch_script(job_dir, script_path)
+        return True
 
