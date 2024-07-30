a95550621f3e13922d3d607889d7ecf1b904b76b
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-07-29 17:08:46 -0700

    remove environmental variables

--- submit_job.py
+++ submit_job.py
@@ -6,8 +6,7 @@ from util import clean_up, log_error
 
 nstasks = 1
 max_walltime = "00:05"
-psi4_env_dir = os.environ.get("PSI4_ENV_DIR")
-qcEngine_script_loc = os.environ.get("QCENGINE_SCRIPT_LOC")
+qcEngine_script_loc = "./run_qcengine.py"
 
 # TODO: Add S3 Upload
 # TODO: Modify code for Psi4 and QCEngine
@@ -48,7 +47,6 @@ def write_sbatch_script(job_dir: str, script_path: str, job_input_data: dict):
     module load openmpi/4.1.5
     module load psi4/1.9
 
-    source {psi4_env_dir}
     basisSet="{job_basis_set}"
     method="{job_theory}"
     calculationType="{job_calculation_type}"
